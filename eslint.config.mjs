d6a465c44bdaea5912d928fef8aa7afea46b8018
paul <paul.panc@protonmail.ch>
2026-03-05 11:39:47 +0200

    fix: Fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.ts'],
     rules: {
